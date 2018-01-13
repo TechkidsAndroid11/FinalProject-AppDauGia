5c6751ad298f0815e11e4f73fb109d7f2b715aef
ADMIN\Trần_Tân <tanmhtb@gmail.com>
2018-01-13 20:57:18 +0700

    màn hình chi tiểt đấu giá chủ khách

---
+++ ShelfApp/app/src/main/java/com/example/haihm/shelf/activity/AuctionDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.haihm.shelf.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haihm.shelf.R;
+
+public class AuctionDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auction_details);
+    }
+}
