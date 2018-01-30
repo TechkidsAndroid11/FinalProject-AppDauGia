3436aba80ee1ae578ea1132e24e47e33a6a5dacf
thenam1906 <thangpqse04900@fpt.edu.vn>
2018-01-29 22:23:29 -0800

    done storage

--- ShelfApp/app/src/main/java/com/example/haihm/shelf/adapters/ViewPagerProfileAdapter.java
+++ ShelfApp/app/src/main/java/com/example/haihm/shelf/adapters/ViewPagerProfileAdapter.java
@@ -3,12 +3,15 @@ package com.example.haihm.shelf.adapters;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by ThangPham on 1/16/2018.
  */
 
 public class ViewPagerProfileAdapter extends FragmentPagerAdapter {
+    private static final String TAG = "ViewPagerProfileAdapter";
     public ViewPagerProfileAdapter(FragmentManager fm) {
         super(fm);
     }
@@ -19,10 +22,12 @@ public class ViewPagerProfileAdapter extends FragmentPagerAdapter {
         {
             case 0:
             {
+                Log.d(TAG, "getItem: ");
                 break;
             }
             case 1:
             {
+                Log.d(TAG, "getItem: ");
                 break;
             }
         }
